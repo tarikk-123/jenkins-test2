a770b01bbb88275764d02b8c87c725ee2a1e7ca8
Tarık Yılmaz <159800974+tarikk-123@users.noreply.github.com>
2025-06-10 11:08:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Merhaba, Dünya!, bu 2. versiyon '
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
